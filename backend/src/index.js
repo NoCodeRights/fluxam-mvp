8dbd4d9a5b262964dc200d17be5c2ad9a3e84d46
Marcelo Sepulveda <marcelo.sepulveda.ramirez@gmail.com>
2025-07-27 19:45:44 -0400

    feat: add root healthcheck route

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,10 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.get('/', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.use(cors());
 app.use(express.json());
 
